8f87dc2ebe0cf31de30fdff4269a3c4c46528ef7
Jon Atxa <atxajon@gmail.com>
2019-08-12 16:39:06 +0200

    home page test

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
-  constructor() {}
+  
+  text = 'Default text';
+  onChangeText() {
+    this.text = 'Changed text';
+  }
 
 }
