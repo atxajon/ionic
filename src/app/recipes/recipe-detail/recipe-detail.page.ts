205119f22eaedadbf46b4740e850541b53f542fe
Jon Atxa <atxajon@gmail.com>
2019-09-06 12:41:36 +0200

    added deletion of a recipe

--- src/app/recipes/recipe-detail/recipe-detail.page.ts
+++ src/app/recipes/recipe-detail/recipe-detail.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Recipe } from '../recipe.model';
 import { RecipesService } from '../recipes.service';
 
@@ -13,7 +13,8 @@ export class RecipeDetailPage implements OnInit {
   // Inject from imported angular's ActivatedRoute on line 2.
   constructor(
     private activatedRoute: ActivatedRoute, 
-    private recipesService: RecipesService) { }
+    private recipesService: RecipesService,
+  private router: Router) { }
 
   ngOnInit() {
     // paramMap is a map of all the parameters this route receives. Its' an observable. In subscribe we pass in a function that executes when we receive the data, the function will get a paramMap obj that we can retrieve the parameters from.
@@ -25,6 +26,12 @@ export class RecipeDetailPage implements OnInit {
       const recipeId = paramMap.get('recipeId');
       this.loadedRecipe = this.recipesService.getRecipe(recipeId);
     });
+  }
+
+  onDeleteRecipe() {
+    this.recipesService.deleteRecipe(this.loadedRecipe.id);
+    // After deleting navigate away to /recipes.
+    this.router.navigate(['/recipes']);
   }
 
 }
