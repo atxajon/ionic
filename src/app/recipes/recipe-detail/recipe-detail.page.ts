7250dcf221909d8c2bacccfedc79a81cae2cfb95
Jon Atxa <atxajon@gmail.com>
2019-09-16 10:12:54 +0200

    deletion of a recipe now prompts a message on screen through AlertController

--- src/app/recipes/recipe-detail/recipe-detail.page.ts
+++ src/app/recipes/recipe-detail/recipe-detail.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Recipe } from '../recipe.model';
 import { RecipesService } from '../recipes.service';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -14,13 +15,15 @@ export class RecipeDetailPage implements OnInit {
   constructor(
     private activatedRoute: ActivatedRoute, 
     private recipesService: RecipesService,
-  private router: Router) { }
+    private router: Router,
+    private alertCtrl: AlertController) { }
 
   ngOnInit() {
     // paramMap is a map of all the parameters this route receives. Its' an observable. In subscribe we pass in a function that executes when we receive the data, the function will get a paramMap obj that we can retrieve the parameters from.
     this.activatedRoute.paramMap.subscribe(paramMap => {
       if (!paramMap.has('recipeId')) {
         // Redirect.
+        this.router.navigate(['/recipes']);
         return;
       }
       const recipeId = paramMap.get('recipeId');
@@ -29,9 +32,24 @@ export class RecipeDetailPage implements OnInit {
   }
 
   onDeleteRecipe() {
-    this.recipesService.deleteRecipe(this.loadedRecipe.id);
-    // After deleting navigate away to /recipes.
-    this.router.navigate(['/recipes']);
+    this.alertCtrl.create({
+      header: 'Are you sure?', 
+      message: 'Do you really want to delete the recipe?', 
+      buttons: [{
+        text: 'Cancel',
+        role: 'cancel'
+      }, {
+        text: 'Delete',
+        handler: () => {
+          this.recipesService.deleteRecipe(this.loadedRecipe.id);
+          // After deleting navigate away to /recipes.
+          this.router.navigate(['/recipes']);
+        }
+      }
+    ]
+    }).then(alertEl => {
+      alertEl.present();
+    });
   }
 
 }
